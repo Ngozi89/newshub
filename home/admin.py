b69f3c985f44358c44b821f8a5c356e3abeb7191
Ngozi Omenka <ngozi.madu@live.se>
2024-09-15 14:22:28 +0000

    Addd comment admin to enable damin manage comment

--- home/admin.py
+++ home/admin.py
@@ -1,10 +1,22 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
 # Register post models and post admin class.
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
+    list_display = ('title', 'slug', 'status', 'created_on')
+# Search field.
+    search_fields = ['title', 'content']
+    list_filter = ('status', 'created_on')
 
+    list_filter = ('status', 'created_on')
     summernote_fields = ('content')
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    """Allows admin to mamage comments on posts via the admin panael"""
+    list_display = ('name', 'body', 'post', 'created_on')
+    list_filter = ('created_on',)
+    search_fields = ['name', 'email', 'body']
