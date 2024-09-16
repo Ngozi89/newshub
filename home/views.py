6f4cd201c89896af591e4289234b209c1eaacde3
Ngozi Omenka <ngozi.madu@live.se>
2024-09-16 15:20:25 +0000

    created starter templates and add base.html file, index and imported static file

--- home/views.py
+++ home/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import generic, View
+from .models import Post
+
 
 # Create your views here.
+class PostList(generic.ListView):
+    model = Post
+# Render list of post
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+# Seperate the pages
+    paginate_by = 8
