af9a4b0e74aaab45d6719716d8ff6b554f19cc12
Ngozi Omenka <ngozi.madu@live.se>
2024-09-17 21:48:15 +0000

    Added post details view and in  template

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
     path('news/', views.PostList.as_view(), name='news'),
+    path('postdetail/<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic, View
 from .models import Post
 
@@ -16,3 +16,24 @@ class PostList(generic.ListView):
     template_name = 'news.html'
 # Seperate the pages
     paginate_by = 8
+
+
+class PostDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.order_by('created_on')
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "post_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "liked": liked
+            },
+        )
